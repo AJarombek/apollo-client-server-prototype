439a5067bdb4f99c0f21a41d283687ed72a5a450
Andrew Jarombek <ajarombek95@gmail.com>
2020-08-19 20:09:56 -0400

    flower details

--- client/src/components/FlowerDetails/FlowerDetails.tsx
+++ client/src/components/FlowerDetails/FlowerDetails.tsx
@@ -6,9 +6,10 @@
 
 import React from 'react';
 import {FlowerData} from "../../types";
-import {AJModal} from "jarombek-react-components";
+import {AJButton, AJModal} from "jarombek-react-components";
 import gql from "graphql-tag";
 import {useQuery} from "react-apollo";
+import classNames from "classnames";
 
 const GET_FLOWER_DETAILS = gql`
     query flowerDetails($id: ID!) {
@@ -43,10 +44,39 @@ const FlowerDetails: React.FunctionComponent<IProps> = ({ flowerId }) => {
                         <div className="header-info">
                             <p className="type">{flower.type}</p>
                             <p className="spread">&#x5e;</p>
-                            <p className="price">
-                                {flower.onSale ? flower.salePrice : flower.price}
+                            <p
+                                className={classNames(
+                                    "price", flower.onSale ? "salePrice" : "normalPrice"
+                                )}
+                            >
+                                ${flower.onSale ? flower.salePrice : flower.price}
                             </p>
                         </div>
+                    </div>
+                    <div className="body">
+                        <div className="status">
+                            {flower.count > 0 ? (
+                                <>
+                                    <p className="in-stock">&#xe052;</p>
+                                    <p>In Stock</p>
+                                </>
+                            ): (
+                                <>
+                                    <p className="out-of-stock">&#xe051;</p>
+                                    <p>Out of Stock</p>
+                                </>
+                            )}
+                        </div>
+                        <p>Quantity: {flower.count}</p>
+                        <p>{flower.description}</p>
+                    </div>
+                    <div className="footer">
+                        <AJButton type="contained" onClick={() => {}} disabled={false}>
+                            Add to Cart
+                        </AJButton>
+                        <AJButton type="text" onClick={() => {}} disabled={false}>
+                            Return
+                        </AJButton>
                     </div>
                 </div>
             )}
