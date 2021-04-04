97f0d45cc330b3fd9ca47ea29a1ccf7ed705894f
ajarombek <ajarombek95@gmail.com>
2021-04-03 22:10:08 -0400

    image snapshot tests working

--- client/test/checkout.test.ts
+++ client/test/checkout.test.ts
@@ -4,7 +4,7 @@
  * @since 4/3/2021
  */
 
-import puppeteer from 'puppeteer';
+import puppeteer, { ElementHandle } from 'puppeteer';
 
 describe('Snapshot Image tests for the checkout page.', () => {
   let browser: puppeteer.Browser;
@@ -19,6 +19,31 @@ describe('Snapshot Image tests for the checkout page.', () => {
     const image = await page.screenshot();
 
     expect(image).toMatchImageSnapshot();
+  });
+
+  it('displays items in the cart correctly', async () => {
+    const page = await browser.newPage();
+    await page.goto('http://localhost:8082/');
+    const addToCartButtons: ElementHandle[] = await page.$$('.add-to-cart');
+    await addToCartButtons[0].click();
+    await addToCartButtons[1].click();
+    await addToCartButtons[2].click();
+    await addToCartButtons[3].click();
+
+    const cartIcon: ElementHandle = await page.$('.cart');
+    await cartIcon.click();
+    await page.waitForSelector('.checkout-item');
+    await page.waitForTimeout(500);
+
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot({ customDiffConfig: { threshold: 0.3 } });
+
+    await page.$eval('.grand-total', (e) => {
+      e.scrollIntoView();
+    });
+
+    const imageBottomOfPage = await page.screenshot();
+    expect(imageBottomOfPage).toMatchImageSnapshot();
   });
 
   afterAll(async () => {
--- client/test/storefront.test.ts
+++ client/test/storefront.test.ts
@@ -4,7 +4,7 @@
  * @since 4/3/2021
  */
 
-import puppeteer from 'puppeteer';
+import puppeteer, { ElementHandle } from 'puppeteer';
 
 describe('Snapshot Image tests for the storefront page.', () => {
   let browser: puppeteer.Browser;
@@ -13,12 +13,35 @@ describe('Snapshot Image tests for the storefront page.', () => {
     browser = await puppeteer.launch();
   });
 
-  it('displays the storefront page correctly', async () => {
+  it('displays page correctly', async () => {
     const page = await browser.newPage();
     await page.goto('http://localhost:8082/');
     const image = await page.screenshot();
 
     expect(image).toMatchImageSnapshot();
+  });
+
+  it('displays page with items in cart correctly', async () => {
+    const page = await browser.newPage();
+    await page.goto('http://localhost:8082/');
+    const addToCartButtons: ElementHandle[] = await page.$$('.add-to-cart');
+    await addToCartButtons[0].click();
+    await addToCartButtons[1].click();
+    await addToCartButtons[2].click();
+    await addToCartButtons[3].click();
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('displays flower modals correctly', async () => {
+    const page = await browser.newPage();
+    await page.goto('http://localhost:8082/');
+    const flowerCardTitle: ElementHandle = await page.$('.flower-card > img');
+    await flowerCardTitle.click();
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot({ customDiffConfig: { threshold: 0.3 } });
   });
 
   afterAll(async () => {
